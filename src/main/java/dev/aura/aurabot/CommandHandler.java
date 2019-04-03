540850d0e8d8abf9199eecc377310c5a312de4f5
Ally <sonically@hotmail.co.uk>
2019-04-03 16:57:24 +0100

    Added an 'a: embed' command for message embeds

--- src/main/java/dev/aura/aurabot/CommandHandler.java
+++ src/main/java/dev/aura/aurabot/CommandHandler.java
@@ -2,9 +2,14 @@ package dev.aura.aurabot;
 
 import com.jagrosh.jdautilities.command.CommandClient;
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import dev.aura.aurabot.commands.EmbedCMD;
 
 public class CommandHandler {
   public static CommandClient getClient() {
-    return new CommandClientBuilder().setOwnerId("158653179041546242").setPrefix(":a ").build();
+    return new CommandClientBuilder()
+            .setOwnerId("158653179041546242")
+            .setPrefix(":a")
+            .addCommand(new EmbedCMD())
+            .build();
   }
 }
